f0ff2647592e9e2ebb485a894b27599ce58da204
a.cabrera <aranza@CB.localdomain>
2022-01-12 16:01:32 +0000

    Project structure

---
+++ catalog/urls.py
@@ -0,0 +1,17 @@
+from django.urls import include, re_path
+
+from . import views
+
+
+urlpatterns = [
+
+]
+
+# from django.urls import include, re_path
+
+# from myapp.views import home
+
+# urlpatterns = [
+#     re_path(r'^$', home, name='home'),
+#     re_path(r'^myapp/', include('myapp.urls'),
+# ]
