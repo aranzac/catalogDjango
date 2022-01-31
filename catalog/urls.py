7ebdd20d1e8121172b42bbea9930d4d6658bd3d8
a.cabrera <aranza@CB.localdomain>
2022-01-31 19:19:33 +0000

    Generic lists and views

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,17 +1,8 @@
 from django.urls import include, re_path
-
 from . import views
 
-
 urlpatterns = [
-
+    re_path(r'^$', views.index, name='index'),
+    re_path(r'^books/$', views.BookListView.as_view(), name='books'),
+    re_path(r'^book/(?P<pk>\d+)$', views.BookDetailView.as_view(), name='book-detail'),
 ]
-
-# from django.urls import include, re_path
-
-# from myapp.views import home
-
-# urlpatterns = [
-#     re_path(r'^$', home, name='home'),
-#     re_path(r'^myapp/', include('myapp.urls'),
-# ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,3 +1,44 @@
+from .models import Book, Author, BookInstance, Genre
 from django.shortcuts import render
+from django.views import generic
 
-# Create your views here.
+def index(request):
+    """
+    Función vista para la página inicio del sitio.
+    """
+    
+    # Genera contadores de algunos de los objetos principales
+    num_books=Book.objects.all().count()
+    num_instances=BookInstance.objects.all().count()
+
+    # Libros disponibles (status = 'a')
+    num_instances_available=BookInstance.objects.filter(status__exact='a').count()
+    num_authors=Author.objects.count()  # El 'all()' esta implícito por defecto.
+
+    # Renderiza la plantilla HTML index.html con los datos en la variable contexto
+    return render(
+        request,
+        'index.html',
+        context={'num_books':num_books,'num_instances':num_instances,'num_instances_available':num_instances_available,'num_authors':num_authors},
+    )
+
+class BookListView(generic.ListView):
+    model = Book
+
+    def get_queryset(self):
+        return Book.objects.filter(title__icontains='war')[:5] # Get 5 books containing the title war´
+
+class BookDetailView(generic.DetailView):
+    """Generic class-based detail view for a book."""
+    model = Book
+
+
+class AuthorListView(generic.ListView):
+    """Generic class-based list view for a list of authors."""
+    model = Author
+    paginate_by = 10
+
+
+class AuthorDetailView(generic.DetailView):
+    """Generic class-based detail view for an author."""
+    model = Author
